5b88738a0854e75f806aaf3e61fe67a9499d2753
orlandomarin <orlandomarin@orlandos-air.lan>
2024-06-04 00:00:44 -0400

    Fourth Python Program - Define and call greeting function

---
+++ main.py
@@ -0,0 +1,14 @@
+# Write a function called greetUserByName() that prompts the user for his / her
+# name and displays the message "Hello x" where x represents the name entered
+# by the user. After coding the function, write a statement to call 
+# the function.
+
+# define the greetUserByName function
+def greetUserByName():
+    name = input("Hello! What is your name?\n")
+    print("Hello, " + name + "!")
+
+
+# call greetUserByName function
+greetUserByName()
+    
